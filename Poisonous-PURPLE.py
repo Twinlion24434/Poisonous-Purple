6547b682a451a556d9ca8d0e632e37bbd720522d
Twinlion_TL <150567863+Twinlion24434@users.noreply.github.com>
2025-05-23 17:25:34 +0700

    Update Poisonous-PURPLE.py

--- Poisonous-PURPLE.py
+++ Poisonous-PURPLE.py
@@ -98,9 +98,9 @@ Birruh Bidam Nafdika  Yaa  Aqsa
 ||============================ 𒈞 By Khanza 𒈞 ===========================||                                                          
 ''' 
 
-red_art2 = f"{Fore.RED}{art2}{Style.RESET_ALL}"
+red_art2 = f"{Fore.MAGENTA}{art2}{Style.RESET_ALL}"
 print(red_art2.center(80))
-print(Fore.RED + Style.BRIGHT + "[Khanza's dedication and struggle for PALISAINE]" + Style.RESET_ALL)
+print(Fore.MAGENTA + Style.BRIGHT + "[Khanza's dedication and struggle for PALISAINE]" + Style.RESET_ALL)
 
 def menu():
    # print(Style.BRIGHT + Fore.YELLOW + "[INFO!]" Fore.WHITE + "Press CTRL + C and press enter to exit!!")
