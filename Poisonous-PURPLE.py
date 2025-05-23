f7892915f62e47d12486daebe20695e6c6340a76
Twinlion_TL <150567863+Twinlion24434@users.noreply.github.com>
2025-05-23 18:24:00 +0700

    Update Poisonous-PURPLE.py

--- Poisonous-PURPLE.py
+++ Poisonous-PURPLE.py
@@ -98,8 +98,9 @@ Birruh Bidam Nafdika  Yaa  Aqsa
 ||============================ 𒈞 By Khanza 𒈞 ===========================||                                                          
 ''' 
 
-art2 = "POISONOUS PURPLE"  # Definisikan art2
-red_art2 = f"{Fore.RED}{art2}{Style.RESET_ALL}"  # Gunakan art2
+art2 = "Poisonous Purple"  # Define art2
+red_art2 = f"{Fore.RED}{art2}{Style.RESET_ALL}"  # Use art2 with red color
+print(red_art2)
 print(red_art2.center(80))
 print(Fore.RED + Style.BRIGHT + "[Khanza's dedication and struggle for PALISAINE]" + Style.RESET_ALL)
 
